e68bf9057a4647a083bb1a374097dfe63603e689
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-06-27 10:45:15 +0200

    Character creation

---
+++ script.js
@@ -0,0 +1,12 @@
+// i = 0;
+// function font() {
+//   if (i == 0) {
+//     i++;
+//     document.querySelector("#text").style.fontSize = "35px";
+//   } else {
+//     i--;
+//     document.querySelector("#text").style.fontSize = "16px";
+//   }
+// }
+
+let room = 0;
