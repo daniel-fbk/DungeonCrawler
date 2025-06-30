476a57bd0f93134b145d93513188cc1a854535ba
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-06-30 15:03:52 +0200

    Added entrance section

--- script.js
+++ script.js
@@ -9,4 +9,30 @@
 //   }
 // }
 
+/* 
+---Dungeon Crawler---
+
+"Health" start at "50"
+Start at "Character Creation"
+Change to "Dungeon" when button is clicked and name has input
+Start in room one, Default background-image
+Choose between left, right or forward room
+Randomize event upon entering room, change background-image depending on triggered event
+
+Events:
+Nothing
+Fountain, heals an amount of "Health"
+Skeleton, option to attempt to flee or fight, lose "Health", chance to avoid losing "Health" if flee is effective
+
+Find "Exit" to win
+
+*/
+
 let room = 0;
+let health = 50;
+
+function switchHub() {
+  document.getElementById("character-creation").style.display = "none";
+
+  document.getElementById("dungeon").style.display = "flex";
+}
