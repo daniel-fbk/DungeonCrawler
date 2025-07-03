e6246696a4dafc0c2a6523064a9c73305c546776
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-07-03 10:14:59 +0200

    Adding functions of repetitive code

--- script.js
+++ script.js
@@ -23,64 +23,86 @@ let health = 50;
 let fleeChance = 45;
 let eventRoll = 0;
 
+const healthDisplay = document.getElementById("health");
+const roomCount = document.querySelector("#room");
+const changeImage = document.querySelector("#room-image");
+const roomPrompt = document.querySelector("#room-prompt");
+
+// const controls = document.querySelector("controls");
+
+// function showControls() {
+//   document.querySelector(".controls").style.display = "grid";
+// }
+
+// function hideControls() {
+//   document.querySelector(".controls").style.display = "none";
+// }
+
 function switchHub() {
   document.getElementById("character-creation").style.display = "none";
   document.getElementById("dungeon").style.display = "flex";
-  document.getElementById("forward").style.display = "inline-block";
+  document.getElementById("forward").style.display = "block";
   let userName = document.getElementById("name-input").value;
 }
 
 function nextRoom() {
   if (room == 0) {
-    room++;
-    document.getElementById("left").style.display = "inline-block";
-    document.getElementById("right").style.display = "inline-block";
-    document.getElementById("room-image").src =
-      "./assets/images/dungeon-hallway.jpg";
-    document.getElementById("room-prompt").textContent =
+    incrementRoom();
+    document.getElementById("left").style.display = "block";
+    document.getElementById("right").style.display = "block";
+    changeImage.src = "./assets/images/dungeon-hallway.jpg";
+    roomPrompt.textContent =
       "You step into a damp stone chamber. The air is thick, and the walls echo with distant dripping water. There are three narrow passageways: one to the left, one straight ahead, and one to the right. Which way do you go?";
-    document.getElementById("room").textContent = "Room 1";
   } else if (room == 1) {
-    room++;
+    incrementRoom();
     treasureChest();
   }
 }
 
-function showControls() {
-  document.querySelector("controls").style.display = "grid";
-}
-
-function hideControls() {
-  document.querySelector("controls").style.display = "none";
-}
-
 function treasureChest() {
-  document.getElementById("room-image").src = "./assets/images/chest.jpg";
-  document.getElementById("room-prompt").textContent =
-    "You see a chest in front of you. It looks old, but untouched.";
+  changeImage.src = "./assets/images/chest.jpg";
+  roomPrompt.textContent = "You find a chest. It looks old, but untouched.";
   document.querySelector(".controls").style.display = "none";
+  document.getElementById("chest-options").style.display = "block";
 }
 
 function openChest() {
   eventRoll = randomizeNum(7);
   console.log(eventRoll);
   if (eventRoll <= 3) {
-    document.getElementById("room-prompt").textContent = "The chest was empty";
+    roomPrompt.textContent = "The chest was empty.";
   } else if (eventRoll == 4) {
     fleeChance += 10;
-    document.getElementById("room-prompt").textContent =
+    roomPrompt.textContent =
       "You find an old pair of leather boots. This might be useful when fleeing.";
   } else if (eventRoll == 5) {
     skipRoomPotion++;
-    document.getElementById("room-prompt").textContent =
-      "You found a Skip Potion! Use this to skip a room";
+    roomPrompt.textContent =
+      "You found a Skip Potion! Use this to skip a room.";
   } else if (eventRoll == 6) {
-    health -= 10;
-    document.getElementById("room-prompt").textContent = "A mimic!";
+    changeHealth("-", 10);
+    roomPrompt.textContent = "A mimic!";
     document.getElementById("room-image").src = "./assets/images/mimic.jpg";
+    console.log(`Current Health: ${health}`);
   }
 }
 
 function randomizeNum(num1) {
   return Math.floor(Math.random() * num1);
 }
+
+function changeHealth(operator, healthValue) {
+  switch (operator) {
+    case "+":
+      healthDisplay.textContent = `Health: ${health.toString()}`;
+      return (health += healthValue);
+    case "-":
+      healthDisplay.textContent = `Health: ${health.toString()}`;
+      return (health -= healthValue);
+  }
+}
+
+const incrementRoom = () => {
+  room++;
+  roomCount.textContent = `Room: ${room}`;
+};
